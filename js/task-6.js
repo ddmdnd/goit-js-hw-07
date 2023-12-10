7ffcfe3c3e32a8514e72a654ab896e8d53d989d6
ddmdnd <139027634+ddmdnd@users.noreply.github.com>
2023-12-10 13:47:03 -0500

    summary

--- js/task-6.js
+++ js/task-6.js
@@ -5,31 +5,31 @@ const btmDestroy = document.querySelector('[data-destroy]');
 const boxesForAdd = document.querySelector("#boxes");
 btmCreate.addEventListener("click", addDiv);
 btmDestroy.addEventListener("click", deleteDiv);
+btmCreate.style.background = "#4E75FF";
+btmDestroy.style.background = "#FF4E4E";
 function addDiv(){
     boxesForAdd.innerHTML = "";
     const inputData = input.value;
     let widthBoxChild = 20;
 for (let i=0; i< inputData; i++){
-    widthBoxChild += 10;
-    const divAdd = `<div width = "${widthBoxChild.toString()}" hight = "${widthBoxChild.toString()}">1</div>`;
+    // widthBoxChild += 10;
+    const divAdd = `<div></div>`;
 boxesForAdd.insertAdjacentHTML("beforeend", divAdd)
-const divBackgroundColor = document.querySelectorAll("#boxes > div")
-divBackgroundColor.forEach(element => {
-    element.style.background = getRandomHexColor();
-});
 }
-
+input.value ="";
+const divBackgroundColor = document.querySelectorAll("#boxes > div");
+for( const div of divBackgroundColor){
+widthBoxChild += 10;
+div.style.width = `${widthBoxChild}px`;
+div.style.height = `${widthBoxChild}px`;
+div.style.background = getRandomHexColor();
+}
+}
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
       .toString(16)
       .padStart(6, 0)}`;
   }
-}
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 function deleteDiv(){
     boxesForAdd.innerHTML = "";
 }
--- js/task-7.js
+++
@@ -1,4 +0,0 @@
-// const article =
-// document.querySelector(" .article");
-// const htmlString = '<p class="article-text">Nullam quis ante. Vestibulum dapibus nunc ac augue. In consectetuer turpis ut velit.</p> <a class="link" href="#">Read more...</a>;'
-// article.innerHTML += htmlString;
